1a409acdd63a61d100cd7325f40401afa4e082cf
gastonbrizuela <gastonbrizuela@gmial.com>
2022-02-10 17:44:41 -0300

    modelo, rutas, controler del usuario

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,11 +1,10 @@
 const UserModel = require('../models/user');
 const HttpException = require('../utils/HttpException.utils');
 // const { validationResult } =require('express-validator');
-// const bcrypt = require('bcrypt');
-//const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 // const deteven = require('dotenv');
 // dotenv.config()
-
 
 class UserController {
     getAllUser = async (req, res, next)=>{
@@ -15,19 +14,72 @@ class UserController {
     }
 
     getUserById = async (req, res, next)=>{
-        console.log('obtener usuario por su id')
+        const {nombre, apellido, mail} = req.body
+        const user = new User({nombre, apellido, mail})
+        result = await user.save()
+        if (!result){
+            throw new HttpException(500, 'Something went wrong');
+        }
+        res.json({status: 'Usuario Guardado'})
     }
 
     createUser = async (req, res, next)=>{
-        console.log('crear un usuario')
+        //try{
+            console.log(req.body)
+            await this.hashPassword(req);
+            console.log(req.body)
+            const {nombre, apellido,mail, password} = req.body;
+            const user = new UserModel({nombre, apellido, mail,password});
+            await user.save();
+            res.json({status: 'Usuario Guardado'})
+   // }catch(err){
+            //res.status(500).json({status:'Something went wrong', "error": err})
+      //  }
     }
+    
 
     deleteUser = async (req, res, next)=>{
-        console.log('eliminar un usuario')
+        //try{
+            await UserModel.findByIdAndRemove(req.params.id)
+            res.status(200).json({message:`Se elimino correctamente el usuario ${req.params.id}`})
+        //}catch(err){
+        //    res.json({status:'Ocurrio un error al eliminar', error: err})
+       // }
     }
 
     updateUser = async (req, res, next)=>{
-        console.log('modificar un usuario')
+        try{
+            const {nombre, apellido, mail} = req.body
+            const putUser = {nombre, apellido}
+            await User.findByIdAndUpdate(req.params.id, putUser);
+            res.status(200).json({status:`se actualizo usuario ${req.params.id}`})
+            } catch(err){
+                console.log(err); // TypeError: failed to fetch
+                res.json({status:'Se genero un error', error: err})
+            }
+        
+    }
+    userLogin = async (req, res, next)=>{
+        const {mail, password: pass} = req.body;
+        const user = await UserModel.findOne({"mail": mail}).exec();
+        console.log('pass')
+        console.log(mail)
+        console.log('user.password')
+        console.log(user)
+        if (!user){
+            res.status(401).json({"status": "User not found"})
+        }
+        const isMatch = await bcrypt.compare(pass, user.password)
+        if (!isMatch){
+            res.status(401).json({"status": "password Incorrect"})
+        }
+        res.status(200).json({"status": "password correct"})
+    }
+
+    hashPassword = async (req) => {
+        if (req.body.password) {
+            req.body.password = await bcrypt.hash(req.body.password, 8);
+        }
     }
 }
 
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 
 const UserSchema = new Schema({
     nombre: { type: String, required:true },
+    password: { type: String, required:true},
     apellido: {type: String, required: true},
     mail: {type: String, required: true}
 });
